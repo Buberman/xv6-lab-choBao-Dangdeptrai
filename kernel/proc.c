efdb18c78f1539f7e184741326c18570ccb1dba6
Buberman <iambuberman@gmail.com>
2024-03-23 20:02:00 +0700

    Update proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -309,7 +309,7 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
-  np->trace = p->syscall_trace;
+  np->trace = p->trace;
   pid = np->pid;
 
   release(&np->lock);
